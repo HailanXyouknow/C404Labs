84ef00b7bc678dc3c05eef4bde278a1c1c3d9add
HailanXyouknow <hailan@ualberta.ca>
2021-01-11 17:50:30 -0700

    prints out source code

--- lab1.py
+++ lab1.py
@@ -1,8 +1,12 @@
 # Python script that prints out the version of the requests library
 import requests
 
-print(requests.__version__)
+print('requests version:', requests.__version__)
 
-request = requests.get('http://google.com/', allow_redirects=True)
-print(request.status_code)
-print(request.text)
+request = requests.get('http://google.com/')
+print('Status Code (Google):', request.status_code)
+
+request_code = requests.get('https://raw.githubusercontent.com/HailanXyouknow/C404Labs/main/lab1.py')
+print('Status Code (GitHub Source Code):', request_code.status_code)
+print('Source Code (GitHub Source Code):')
+print(request_code.text)
